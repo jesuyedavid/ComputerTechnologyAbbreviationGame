65496e37eea1dd5dbd69a7ae88efd47390189d09
unknown <jesuyed@yahoo.com>
2017-10-12 05:48:19 -0400

    Complete work

---
+++ public_html/ans.php
@@ -0,0 +1,11 @@
+<?php
+
+  if(isset($_POST['name'])===true && empty ($_POST['name'])===false){
+   mysql_connect('mysql9.000webhost.com', 'a6492199_root', 'jesuloba2');
+   mysql_select_db('a6492199_jesuye');
+    $query = mysql_query("select corAns from qanda where question='" .
+    mysql_real_escape_string(trim($_POST['name'])) ."'");
+    echo (mysql_num_rows($query)!==0)?mysql_result($query, 0, 'corAns'):'Name not found';
+  }
+
+?>
